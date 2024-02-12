6b27ed2ae7a2ef90cef3d97115bfd9761992cb2a
Jayabharatha Reddy Sadanagiri <Jayabharathreddys@gmail.com>
2024-02-12 09:36:04 +0530

    first commit

---
+++ src/test/java/com/jb/springjdbcauth/SpringJdbcAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jb.springjdbcauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
